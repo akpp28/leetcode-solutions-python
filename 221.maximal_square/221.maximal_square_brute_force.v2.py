8727c1079a794837ae993e4e38a20c3efd987af7
Akpp Vault <akpp2808@gmail.com>
2026-06-10 15:43:01 +0300

    221 fix typo

--- 221.maximal_square/221.maximal_square_brute_force.v2.py
+++ 221.maximal_square/221.maximal_square_brute_force.v2.py
@@ -7,18 +7,16 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
         max_side_length = 0
-        matrix = [[int(item) for item in row] for row in matrix]
-        print(json.dumps(matrix))
+        matrix = [[int(item) for item in row] for row in matrix] # convert all values to integers
 
         for x in range(rows):
             for y in range(cols):
                 if matrix[x][y] == 0:
                     continue
 
-                # Calculate the maximum boundary limit for the current (x, y)
-                max_limit = min(rows - x, cols - y)
-                for size in range(1, max_limit + 1):
-                    # The "Outer Shell": right wall (X-asis) then bottom floor (Y-asis)
+                max_possible_side_length = min(rows - x, cols - y)
+                for size in range(1, max_possible_side_length + 1):
+                    # The "Outer Shell": right wall (X-axis) then bottom floor (Y-axis)
                     right_wall_ok = all(matrix[next_x][y + size - 1] for next_x in range(x, x + size))
                     bottom_floor_ok = all(matrix[x + size - 1][next_y] for next_y in range(y, y + size))
 
